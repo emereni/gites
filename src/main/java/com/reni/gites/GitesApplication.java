7b088f135edcd38e5b6d449442d1699deec69fcd
Renáta Emmesz <rozsnyakr@gmail.com>
2020-08-24 18:01:42 +0200

    src commit

---
+++ src/main/java/com/reni/gites/GitesApplication.java
@@ -0,0 +1,13 @@
+package com.reni.gites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reni/gites/GitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reni.gites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
